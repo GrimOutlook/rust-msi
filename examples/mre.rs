9fdf4730f264d4147e8654212bb9e6342632cd52
Dominic Grimaldi <dev@grimoutlook.dev>
2025-12-09 14:55:09 -0600

    feat: Update clap for mre example

--- examples/mre.rs
+++ examples/mre.rs
@@ -1,23 +1,40 @@
 use std::{fs::File, io::Write, str::FromStr};
 
-use clap::{App, Arg};
+use clap::{Parser, command};
+use clio::ClioPath;
 use msi::Value;
+use uuid::Uuid;
+
+/// Simple minimum reproducible example showing that a base MSI becomes invalid
+/// after writing a property to the file.
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Filename of the Schema to use
+    #[arg(value_parser = clap::value_parser!(ClioPath))]
+    schema: ClioPath,
+
+    /// Filename of the MSI to create
+    #[arg(value_parser = clap::value_parser!(ClioPath), default_value = "mre.msi")]
+    output: ClioPath,
+
+    /// Revision Number (SummaryInformation property #9) to use for the generated MSI.
+    /// `msitools` doesn't provide a way to set this so during testing, when
+    /// trying to make the MSIs as similar as possible, we may want to set this
+    /// based on external information (e.g an `msitools` generated MSI)
+    #[arg(long, short ,value_parser = clap::value_parser!(Uuid), default_value_t = Uuid::new_v4())]
+    revision_number: Uuid,
+}
 
 fn main() {
-    let matches = App::new("mre")
-        .arg(Arg::with_name("schema").required(true))
-        .arg(Arg::with_name("output").required(true).default_value("out.msi"))
-        .get_matches();
-    let schema = matches.value_of("schema").unwrap();
-    let output = matches.value_of("output").unwrap();
-
-    let mut template = File::open(schema).unwrap();
+    let args = Args::parse();
+    let mut template = File::open(args.schema.to_path_buf()).unwrap();
     let mut file = File::options()
         .create(true)
         .read(true)
         .write(true)
         .truncate(true)
-        .open(output)
+        .open(args.output.to_path_buf())
         .expect("Failed to open broken.msi");
     std::io::copy(&mut template, &mut file).unwrap();
     // Probably not needed but whatever
