ee2ea5f53cbd5b6e5db3ce789ad8d3f493073384
Dominic Grimaldi <self@grimoutlook.dev>
2026-01-08 07:53:40 -0600

    Minor changes to get MRE working and codepage 0 to work

---
+++ examples/mre.rs
@@ -0,0 +1,69 @@
+use std::{fs::File, io::Write, str::FromStr};
+
+use clap::{command, Parser};
+use clio::ClioPath;
+use msi::{CodePage, Delete, Value};
+use uuid::Uuid;
+
+/// Simple minimum reproducible example showing that a base MSI becomes invalid
+/// after writing a property to the file.
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Filename of the Schema to use
+    #[arg(value_parser = clap::value_parser!(ClioPath))]
+    schema: ClioPath,
+
+    /// Filename of the MSI to create
+    #[arg(value_parser = clap::value_parser!(ClioPath), default_value = "mre.msi")]
+    output: ClioPath,
+
+    /// Revision Number (SummaryInformation property #9) to use for the generated MSI.
+    /// `msitools` doesn't provide a way to set this so during testing, when
+    /// trying to make the MSIs as similar as possible, we may want to set this
+    /// based on external information (e.g an `msitools` generated MSI)
+    #[arg(long, short ,value_parser = clap::value_parser!(Uuid))]
+    revision_number: Uuid,
+}
+
+fn main() {
+    let args = Args::parse();
+    let mut template = File::open(args.schema.to_path_buf()).unwrap();
+    let mut file = File::options()
+        .create(true)
+        .read(true)
+        .write(true)
+        .truncate(true)
+        .open(args.output.to_path_buf())
+        .expect("Failed to open broken.msi");
+    std::io::copy(&mut template, &mut file).unwrap();
+    // Probably not needed but whatever
+    file.flush().unwrap();
+
+    let mut package = msi::Package::open(file).expect("Failed to create MSI");
+
+    // Summary Information
+    let sum = package.summary_info_mut();
+    sum.set_uuid(args.revision_number); // PID 9
+    sum.set_codepage(CodePage::from_id(0).unwrap());
+    package.flush().unwrap();
+
+    package.set_database_codepage(msi::CodePage::from_id(0).unwrap());
+    package.delete_rows(Delete::from("Property")).unwrap();
+    // Required Property Information
+    package
+        .insert_rows(msi::Insert::into("Property").rows(vec![
+            vec![Value::from("ProductName"), Value::from("MyProduct")],
+            vec![
+                Value::from("ProductCode"),
+                Value::from("{11111111-1111-1111-1111-111111111113}"),
+            ],
+            vec![Value::from("ProductVersion"), Value::from("0.0.0")],
+            vec![Value::from("ProductLanguage"), Value::from("1033")],
+            vec![Value::from("Manufacturer"), Value::from("MyManufacturer")],
+            vec![Value::from("UpgradeCode"), Value::from("{*}")],
+        ]))
+        .unwrap();
+
+    package.flush().unwrap();
+}
